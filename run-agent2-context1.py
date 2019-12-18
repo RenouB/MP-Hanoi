b261b02adbe5edf213c1a9a3d25f567576f1a3e0
Renou Benteau <abenteau@gmail.com>
2019-12-18 09:28:06 +0100

    modifications to run agent 2

--- run-agent2-context1.py
+++ run-agent2-context1.py
@@ -14,7 +14,7 @@ for agent in [2]:
 	print("AGENT", agent)
 	print("#######################")
 	results[agent] = {}
-	for win_context in [0]:
+	for win_context in [1]:
 		print("\n")
 		print("CONTEXT", win_context)
 		results[agent][win_context] ={}
@@ -45,5 +45,5 @@ for agent in [2]:
 	print("\n")
 
 print(results)
-with open('result-a2c0.pkl', 'wb') as f:
+with open('result-a2c1.pkl', 'wb') as f:
 	pickle.dump(results, f)
